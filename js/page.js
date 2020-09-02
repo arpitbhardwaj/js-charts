4153fd748739ec4373b430d91fccdc7fe942074e
Arpit Bhardwaj <arpit.bhardwaj@sap.com>
2020-09-02 12:52:24 +0530

    Map and Types Arrays

--- js/page.js
+++ js/page.js
@@ -1,102 +1,75 @@
 // Accessing the objects
-let ctx = document.getElementById('monthlySales').getContext('2d');
-let pieCtx = document.getElementById('deptSales').getContext('2d');
-
-let yearlyLabel = document.getElementById('yearlyTotal');
-
-// Values from the form
-let newAmount = document.getElementById('itemAmount');
-let newMonth = document.getElementById('monthId');
-
+var yearlyLabel = document.getElementById('yearlyTotal');
+var newAmount = document.getElementById('itemAmount');
+var newMonth = document.getElementById('monthId');
 let hikingRadio = document.getElementById('hiking');
 let runningRadio = document.getElementById('running');
 let huntingRadio = document.getElementById('hunting');
 
-let yearlyTotal = 0;
+let testBuffer = new ArrayBuffer(16);
 
-const monthlySales = new Set();
-const monthlyLabels = new Set();
+let view1 = new Int16Array(testBuffer);
 
-const categories = new WeakSet();
+view1[0] = 32;
+console.log(view1);
 
-let hiking = { category: "Hiking" };
-let running = { category: "Running" };
-let hunting = { category: "Hunting" };
+let view2 = new DataView(testBuffer);
 
+view2.setInt8(2, 43);
+
+let num = view2.getInt8(2);
+
+let view3 = new DataView(testBuffer);
+
+let num2 = view3.getInt8(2);
+console.log(num2);
+
+let view4 = new DataView(testBuffer, 7, 3);
+view4.setInt8(0, 54);
+view4.setInt8(1, 23);
+view4.setInt8(2, 32);
+
+let numAlert = view4.getInt8(0);
+console.log(numAlert)
+    // Add Sales
 function addSale() {
-    monthlySales.add(parseInt(newAmount.value));
-    monthlyLabels.add(newMonth.value);
 
-    yearlyTotal = 0;
 
-    monthlySalesChart.data.datasets.forEach((dataset) => {
-        dataset.data = [];
-    });
-
-    for (let amount of monthlySales) {
-        yearlyTotal = amount + yearlyTotal;
-        yearlyLabel.innerHTML = yearlyTotal;
-
-        monthlySalesChart.data.datasets.forEach((dataset) => {
-            dataset.data.push(amount)
-        })
-
-    }
-
-    monthlySalesChart.data.labels = Array.from(monthlyLabels);
-
-    monthlySalesChart.update();
-
-    if (hikingRadio.checked) {
-        categories.add(hiking);
-    } else if (runningRadio.checked) {
-        categories.add(running);
-    } else if (huntingRadio) {
-        categories.add(hunting);
-    } else {
-        // Do something
-    }
-
-    console.log(categories);
 
 }
 
-function deletVal() {
-    monthlySales.forEach((value1, value2, monthlySales) => {
-        alert(value1)
-    })
-}
+function findSale() {
 
-function addTotal() {
+
 
 }
 
 // Bar chart
-var monthlySalesChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: [],
-        datasets: [{
-            label: '# of Sales',
-            data: [],
-            backgroundColor: [
-                'rgba(238, 184, 104, 1)',
-                'rgba(75, 166, 223, 1)',
-                'rgba(239, 118, 122, 1)',
-            ],
-            borderWidth: 0
-        }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
+// var monthlySalesChart = new Chart(ctx, {
+//     type: 'bar',
+//     data: {
+//         labels: [],
+//         datasets: [{
+//             label: '# of Sales',
+//             data: [],
+//             backgroundColor: [
+//                 'rgba(238, 184, 104, 1)',
+//                 'rgba(75, 166, 223, 1)',
+//                 'rgba(239, 118, 122, 1)',
+//             ],
+//             borderWidth: 0
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero: true
+//                 }
+//             }]
+//         }
+//     }
+// });
 
 // Pie Chart
 // var deptSalesChart = new Chart(pieCtx, {
